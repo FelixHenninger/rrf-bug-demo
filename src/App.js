f62646ce8123d6c6a74be9a7d1ecf7016a94f8f9
Felix Henninger <mailbox@felixhenninger.com>
2017-01-01 15:08:06 +0100

    Add demo of local form dispatch handlers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { LocalForm, Control } from 'react-redux-form'
+import { LocalForm, Control, actions } from 'react-redux-form'
 
 import GridControl from './components/GridControl'
 import StatePreview from './components/StatePreview'
+
+const addGridRow = (grid) =>
+  [
+    ...grid,
+    Array.apply(null, Array(grid[0].length))
+      .map(String.prototype.valueOf, "")
+  ]
 
 class App extends Component {
   render() {
@@ -18,6 +25,7 @@ class App extends Component {
             type: 'UPDATE_STATE',
             data,
           })}
+          getDispatch={ dispatch => this.formDispatch = dispatch }
         >
           <Control.text model='.title' />
           <GridControl
@@ -28,15 +36,22 @@ class App extends Component {
                 type: 'UPDATE_STATE',
                 data: {
                   ...this.props.state,
-                  grid: [
-                    ...this.props.state.grid,
-                    Array.apply(null, Array(this.props.state.grid[0].length))
-                      .map(String.prototype.valueOf, "")
-                  ]
+                  grid: addGridRow(this.props.state.grid)
                 }
               })
             }
           />
+          <div>
+            <button onClick={ () => this.formDispatch(actions.change('.title', 'I just changed via the form\'s dispatch method!') )}>
+              Change title via LocalForm dispatch
+            </button>
+            <button onClick={ () => this.formDispatch(actions.change('.grid[0][0]', 'Sweet!') )}>
+              Change grid cell via LocalForm dispatch
+            </button>
+            <button onClick={ () => this.formDispatch(actions.change('.grid', addGridRow(this.props.state.grid)) )}>
+              Add grid row via LocalForm dispatch
+            </button>
+          </div>
         </LocalForm>
 
         <h2>State preview</h2>
